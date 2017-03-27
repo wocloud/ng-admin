692e44e164ab6d556c0802bceba78b500f7fad9b
sophia.wang <wangpp@chinaskycloud.com>
2017-03-27 17:12:55 +0800

    0326 : cas

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,9 @@ var gulp = require('gulp'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload');
 
-var src = process.cwd() + '/src';
-var dist = process.cwd() + '/dist';
-
-//extract all dependencies
-gulp.task('extract', function() {
-    
-});
-
-
 //styles
 gulp.task('less', function() {
-    return gulp.src('src/style/*.less')
+    return gulp.src('src/main/webapp/style/*.less')
         .pipe(less())
         .pipe(concat('app.css'))
         .pipe(rename({ suffix: '.min' }))
@@ -33,62 +24,12 @@ gulp.task('less', function() {
         .pipe(gulp.dest('css/'))
         .pipe(notify({ message: 'Less task compvare' }));
 });
-gulp.task('styles', ['less', 'fonts'], function() {
-    return gulp.src('src/assets/css/*.css')
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cleancss())
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(notify({ message: 'Styles task compvare' }));
-});
-
-//fonts
-gulp.task('fonts', function() {
-    return gulp.src('src/assets/fonts/**')
-        .pipe(gulp.dest('dist/assets/fonts'))
-        .pipe(notify({ message: 'Fonts task compvare' }));
-});
-
-// html
-gulp.task('html', function() {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist/'))
-        .pipe(notify({ message: 'html task compvare' }));
-});
-
-//pages
-gulp.task('pages', function() {
-    return gulp.src('src/app/**/*.ts')
-        .pipe(gulp.dest('dist/app/'))
-        .pipe(notify({ message: 'pages task compvare' }));
-});
-
-// clean asserts
-gulp.task('clean', function () {
-    return gulp.src(dist, {read: true}).pipe(clean())
-});
-
-gulp.task('build', ['clean'], function () {
-    gulp.start('styles', 'html', 'scripts', 'pages');
-});
-
-//default task
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'html', 'scripts', 'pages');
-});
 
 //watch all files change
 gulp.task('watch', function() {
 
     gulp.watch('src/app/*.less', ['less']);
 
-    gulp.watch('views/**/*.html');
-
-    //gulp.watch('src/**/.ts', ['scripts']);
-
-    //gulp.watch('src/index.html', ['html']) ;
-
     livereload.listen();
-
-    gulp.watch(['dist/**']).on('change', livereload.changed);
 
 });
---
+++ src/main/webapp/views/instances/vm/vm.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sophia.wang on 17/3/23.
+ */
+'use strict';
+app.controller('VMCtrl', VMCtrl);
+VMCtrl.$inject = ['$scope', 'i18nService'];
+function VMCtrl($scope, i18nService) {
+
+};
